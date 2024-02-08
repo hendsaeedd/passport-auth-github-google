51de18846b5ba341f809108b408fd0add52f0c38
hendsaeedd <hendsaied135@gmail.com>
2024-02-08 13:10:20 +0200

    add nodemailer to registerUser

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,18 +1,46 @@
 import { RequestHandler } from 'express'
 import { User } from '../models/user'
+import { transporter, mailOptions } from './index'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+//function to generate a random token
+const generateToken = () => {
+  return (
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15)
+  )
+}
 
 export const registerUser: RequestHandler = async (req, res) => {
   try {
-    const { name, email } = req.body
+    const { name, email, username } = req.body
 
     const newUser = await User.create({
       name,
       email,
+      username,
     })
+
+    const token = generateToken()
+    await sendConfirmationEmail(email, token)
 
     res.json({ message: 'User was registered successfully', newUser })
   } catch (error: any) {
+    console.error('Error registering user:', error)
     res.status(400).json({ error: error.message })
+  }
+}
+
+export const sendConfirmationEmail = async (email: string, token: string) => {
+  try {
+    const confirmationMailOptions = mailOptions(email, token)
+    await transporter.sendMail(confirmationMailOptions)
+    console.log('Confirmation email sent successfully to:', email)
+  } catch (error) {
+    console.error('Error sending confirmation email:', error)
+    throw new Error('Failed to send confirmation email')
   }
 }
 
